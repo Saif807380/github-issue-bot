31e76326b88e590483eadd36f1d5f620f32053a4
Saif807380 <saif1204kazi@gmail.com>
2021-05-03 18:22:08 +0530

    added naive bayes

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -3,12 +3,14 @@ import Dataset from "../views/Dataset.vue";
 import Overview from "../views/Overview.vue";
 import Tree from "../views/Tree.vue";
 import Forest from "../views/Forest.vue";
+import Bayes from "../views/Bayes.vue";
 
 const routes = [
   { path: "/", component: Overview, meta: { title: "Overview" } },
   { path: "/dataset", component: Dataset, meta: { title: "Dataset" } },
   { path: "/tree", component: Tree, meta: { title: "Decision Tree" } },
   { path: "/forest", component: Forest, meta: { title: "Random Forest" } },
+  { path: "/bayes", component: Bayes, meta: { title: "Naive Bayes" } },
 ];
 
 const router = createRouter({
